c5fa1ce37b5cf5f86c5e2dee113a2fddd33f9dbd
faishalstwn <169064956+faishalstwn@users.noreply.github.com>
2026-05-26 21:12:35 +0700

    Update Counter.java

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -2,26 +2,29 @@ package src.main.java;
 
 public class Counter {
 
-	private int count;
-	
-	public Counter() {
-		this.reset();
-	}
-		
-	public void reset() {
-		count = 0;
-	}
-	
-	public void increment() {
-		count++;
-	}
-	
-	public void decrement() {
-		count--;
-	}
-	
-	public int getCount() {
-		return count;
-	}
-	
+    private int count = 0;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void increment() {
+        count++;
+    }
+
+    public void decrement() {
+        count--;
+    }
+
+    public void increaseBy(int value) {
+        count += value;
+    }
+
+    public void decreaseBy(int value) {
+        count -= value;
+    }
+
+    public void multiplyBy(int value) {
+        count *= value;
+    }
 }
